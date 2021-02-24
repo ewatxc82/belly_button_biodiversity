cccb6c05d3d6cbbc1fc54db8276aa6603512e901
erikwatson <no-reply>
2021-02-23 23:59:55 -0600

    psudeo coded the bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,5 @@
-function readSamples(id)=>{
-    d3.json("../samples.json").then((data)=> {
+function readSamples(id)=> {
+    d3.json("samples.json").then((data)=> {
         var bellydata = data.metadata;
         console.log(bellydata);
         
@@ -17,6 +17,33 @@ function readSamples(id)=>{
     });
 };
 
-//Think I can create a function that includes all info for plots//
+// Creating function for plotly plots
+function plotBuild(id) {
+    //retrieve data from the json file
+    d3.json("samples.json").then( (data)=> {
+        //filter the washing frequency by each id
+        var wfreq = data.metadata.filter(f => f.id.toString() === id)[0];
+        wfreq = wfreq.wfreq;
+        console.log("Washing Frequency:" + wfreq);
+
+        //filter the samples by id
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        console.log("Samples:" + samples)
+
+        //find the top 10 samples 
+        var tTen = samples.sample_values.slice(0, 10).reverse();
+        console.log("a top 10 sample:" + tTen);
+
+        //find tope 10 otu id for otu plot
+        var otuId = (samples.otu_ids.slice(0, 10).reverse();
+        
+        //map the otu ids to make the plot
+        var outIdmap = otuId.map(d => "otu " + d)
+        console.log("otu id's: " + outIdmap);
+        
+    })
+}
+
+
 
 //Need to make init function for the drop down menu//
